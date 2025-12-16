3b0dd4e1192415bb5b95457e9221ad01396cdb75
Mtizen <146360516+Mtizen@users.noreply.github.com>
2025-12-16 11:29:45 +0700

    Revert "[fix] Xóa validate không cần thiết"
    This reverts commit 66ec501f0d960e489442daeb03da1e48202768e2.

--- lib/features/auth/presentation/views/create_class_screen.dart
+++ lib/features/auth/presentation/views/create_class_screen.dart
@@ -168,6 +168,8 @@ class _CreateClassScreenState extends ConsumerState<CreateClassScreen> {
                               validator: (value) {
                                 if (value == null || value.trim().isEmpty)
                                   return 'Tên lớp học không được để trống';
+                                if (value.trim().length < 6)
+                                  return 'Tên lớp phải có ít nhất 6 ký tự';
                                 return null;
                               },
                               decoration: InputDecoration(
